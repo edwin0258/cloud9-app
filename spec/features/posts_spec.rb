a47ffc29e06a07f84ec8311a27d58c9b436e7b4e
edwin0258 <edwin0258@gmail.com>
2015-10-01 17:37:00 +0000

    Add devise, add user and post

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "Display all posts" do
     before do
-        @p1 = Post.create(name: "Hello")
+        @p1 = Post.create(title: "Hello", body: "This is a test post")
     end
     scenario do
         visit "/"
         click_link "posts"
         
         expect(page).to have_content("All posts")
+        expect(page).to have_link(@p1.title)
+        expect(page).to have_content(@p1.body)
     end
 end
